44eed0343582954ee304d59ea263b166c7c9a7f8
lucas boisgard <lucas.boisgard@epitech.eu>
2019-09-27 11:55:06 +0200

    change structure

--- api/graphql/services/UserService/utilities.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-export const dataUser = ({token}) => {
-        const decode = jwt.decode(token);
-        console.log(decode)
-        if (decode != null)
-            return decode.user
-        return false
-}
